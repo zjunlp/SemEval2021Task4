fba3a80f2ca404cafdcbf7ba66de54fd1ec45cc3
cheasim <852975133@qq.com>
2021-01-23 23:48:54 +0800

    update ignore

--- utils/multiple_choices.py
+++ utils/multiple_choices.py
@@ -394,12 +394,12 @@ def convert_examples_to_features(
                 return_overflowing_tokens=True,
                 return_token_type_ids=True,
             )
-            if "num_truncated_tokens" in inputs and inputs["num_truncated_tokens"] > 0:
-                logger.info(
-                    "Attention! you are cropping tokens (swag task is ok). "
-                    "If you are training ARC and RACE and you are poping question + options,"
-                    "you need to try to use a bigger max seq length!"
-                )
+            # if "num_truncated_tokens" in inputs and inputs["num_truncated_tokens"] > 0:
+            #     logger.info(
+            #         "Attention! you are cropping tokens (swag task is ok). "
+            #         "If you are training ARC and RACE and you are poping question + options,"
+            #         "you need to try to use a bigger max seq length!"
+            #     )
 
             choices_inputs.append(inputs)
 
