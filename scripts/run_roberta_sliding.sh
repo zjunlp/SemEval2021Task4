4658e398cf94cb559a63c14bdaeae52ef8d98a45
cheasim <852975133@qq.com>
2021-01-24 13:18:51 +0800

    update script

--- scripts/run_roberta_sliding.sh
+++ scripts/run_roberta_sliding.sh
@@ -5,20 +5,21 @@ SEMEVAL_DIR_TASK1="./dataset/task1"
 SEMEVAL_DIR_TASK2="./dataset/task2"
 # hyperparameter
 # lr = 1e-6 get the result
-learning_rate=5e-6
+learning_rate=3e-6
 epochs=10
 max_seq_length=256
 DATA_DIR=$SEMEVAL_DIR_TASK1
 
-OUTPUT_DIR=./output/${model}_256_task2_sliding_window
-LOGGING_DIR=./logs/${model}_256_task2_sliding_window
+OUTPUT_DIR=./output/${model}_256_task1_sliding_window
+LOGGING_DIR=./logs/${model}_256_task1_sliding_window
 
 
 #  -m torch.distributed.launch --nproc_per_node=1  --nnodes=1\
-CUDA_VISIBLE_DEVICES=1 python \
+CUDA_VISIBLE_DEVICES=2 python \
         run_roberta.py \
         --task_name semeval \
         --sliding_window \
+        --label_smoothing \
         --model_name_or_path ${MODEL_NAME_OR_PATH} \
         --do_train \
         --do_eval \
@@ -28,11 +29,11 @@ CUDA_VISIBLE_DEVICES=1 python \
         --num_train_epochs ${epochs} \
         --max_seq_length ${max_seq_length} \
         --output_dir ${OUTPUT_DIR} \
-        --save_steps 500 \
+        --save_steps 40 \
         --logging_dir $LOGGING_DIR \
-        --eval_steps 500 \
+        --eval_steps 40 \
         --per_device_eval_batch_size=1 \
         --per_device_train_batch_size=1 \
-        --gradient_accumulation_steps 8 \
+        --gradient_accumulation_steps 16 \
         --overwrite_output  \
         --evaluate_during_training    
