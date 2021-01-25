006b96fb76f957ab1d926741f928b06eba863941
cheasim <852975133@qq.com>
2021-01-25 20:59:23 +0800

    v100 files

--- scripts/v100/run_roberta_smoothing.sh
+++ scripts/v100/run_roberta_smoothing.sh
@@ -1,19 +1,18 @@
 model='roberta-large'
 MODEL_NAME_OR_PATH="/home/xx/pretrained_model/"${model}
 # dataset dir
-SEMEVAL_DIR_TASK1="./dataset/task1"
 SEMEVAL_DIR_TASK2="./dataset/task2"
 # hyperparameter
 # lr = 1e-6 get the result
 
-epochs=4
+epochs=15
 max_seq_length=384
 
 
 
 for((i=1;i<=9;i++));
 do
-name=${model}_256_test_label_smoothing_sliding_task1_lr$i
+name=${model}_384_test_label_smoothing_sliding_task2_lr$i
 OUTPUT_DIR=./output/${name}_lr$i
 LOGGING_DIR=./logs/${name}_lr$i
 learning_rate=${i}e-6
@@ -22,12 +21,11 @@ CUDA_VISIBLE_DEVICES= python \
         run_roberta.py \
         --task_name semeval \
         --model_name_or_path ${model} \
-        --sliding \
         --label_smoothing \
         --do_train \
         --do_eval \
         --eval_all_checkpoints \
-        --data_dir $SEMEVAL_DIR_TASK1 \
+        --data_dir $SEMEVAL_DIR_TASK2 \
         --learning_rate ${learning_rate} \
         --num_train_epochs ${epochs} \
         --max_seq_length ${max_seq_length} \
